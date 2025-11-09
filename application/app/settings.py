2d7d8fdb5927634a2d3360bee82ebffb98640589
Fernando Sousa <fernando.sousa@brlink.com.br>
2025-11-09 08:31:58 -0300

    Acertado timezone

--- application/app/settings.py
+++ application/app/settings.py
@@ -140,11 +140,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
